5a47b19973f27d8e40a00ac05606b4a4a3003ae1
Jonathan Lopez <JonL_2086@hotmail.com>
2017-05-02 14:52:16 -0700

    Fixed fancybox gallery
    The two gallery collections weren't separating in fancy box which are
    now separated. Plus, the there were too many images displayed on
    collections page. That has been cutdown to the first image of each
    collection.

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,6 +1,12 @@
-// Protect Images from right-click
-$('[data-fancybox]').fancybox({
-  image : {
-    protect: true
-  }
+// JavaScript Document
+$(function () {
+	"use strict";
+	$('[data-fancybox]').fancybox({
+		image: {
+			protect: true
+		}
+	});
+
+	$("a[rel=blkink]").fancybox();
+	$("a[rel=clrink]").fancybox();
 });
